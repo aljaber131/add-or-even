57da08c4336b2dfacd8bea5d7388ed83c0cee7d1
aljaber131 <60335077+aljaber131@users.noreply.github.com>
2020-02-09 07:55:33 -0800

    Writing a Java to find whether a given number is odd or even

---
+++ odd_or_even.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class odd_or_even {
+
+	public static void main(String[] args) {
+		Scanner havoic = new Scanner(System.in);
+		System.out.println("Give number please!");
+		double b = havoic.nextDouble();
+		if(b%2 == 0) {
+			System.out.println("The number is even");
+		}
+		else {
+			System.out.println("The number is odd");
+		}
+		
+
+	}
+
+}
